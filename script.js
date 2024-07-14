5bf37cc2d9a9c5b61f2b724b5d389e90cafd43c6
huseyineskan01@gmail.com <huseyineskan01@gmail.com>
2024-07-14 03:29:48 +0200

    Fixed a few codes in the JS file

--- script.js
+++ script.js
@@ -1,38 +1,15 @@
-const callStack = [
-  'a(): returns "freeCodeCamp " + b()'
-];
-
-const a = () => {
-  return "freeCodeCamp " + b();
-};
-
-const b = () => {
-  return "is " + c();
-};
-
-const c = () => {
-  return "awesome!";
-};
-
-console.log(a());
-
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  let binary = "";
-
   if (input === 0) {
-    binary = "0";
+    return "0";
+  } 
+  
+  else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
-
-  while (input > 0) {
-    binary = (input % 2) + binary;
-    input = Math.floor(input / 2);
-  }
-
-  result.innerText = binary;
 };
 
 const checkUserInput = () => {
@@ -45,7 +22,7 @@ const checkUserInput = () => {
     return;
   }
 
-  decimalToBinary(parseInt(numberInput.value));
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
   numberInput.value = "";
 };
 
